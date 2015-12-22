ffaf34162d59a095ebb2a72346c360e3394f45c4
Maxwell Weru <mburumaxwell@gmail.com>
2015-12-22 08:50:55 +0300

    added webjob

---
+++ src/TutorialWebjob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorialWebjob {
+    public class Program {
+        public static void Main(string[] args) {
+
+        }
+    }
+}
