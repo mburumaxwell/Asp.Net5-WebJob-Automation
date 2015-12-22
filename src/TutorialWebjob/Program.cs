82f9a1e93366272b2938876b87aeee1fc5ba1b47
Maxwell Weru <mburumaxwell@gmail.com>
2015-12-22 09:03:04 +0300

    guidelines for webjob

--- src/TutorialWebjob/Program.cs
+++ src/TutorialWebjob/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 namespace TutorialWebjob {
     public class Program {
         public static void Main(string[] args) {
-
+            // setup your WebJob the way you want.
+            // For more details see https://azure.microsoft.com/en-us/documentation/articles/web-sites-create-web-jobs/
+            // For specifics of WebJobs in Asp.Net 5 see http://www.heikura.me/2015/8/13/publish-aspnet-5-dnx-console-app-as-azure-webjob
         }
     }
 }
